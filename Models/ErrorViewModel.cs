1738b504f3207ab842b469aef31d01e2e59afa88
Waqas Mumtaz <waqas.mumtaz@synergy-it.com>
2026-01-08 23:31:48 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCicdDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
